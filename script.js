5f0514b61c7a6432568010947d39484697397908
Gyananjay31 <gyananjaydwivedi@gmail.com>
2025-02-24 23:11:59 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var divelement = document.getElementById("myDiv");
+
+divelement.style.color = "blue";    
+divelement.style.backgroundColor = "yellow"; 
+
+divelement.style.margin = "20px";  
+divelement.style.padding = "10px";  
+
+divelement.style.fontSize = "18px";         
+divelement.style.fontWeight = "bold";       
+
+divelement.style.height = "200px";          
+divelement.style.width = "300px";           
